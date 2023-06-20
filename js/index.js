d46cfe635f9cdf2bce5bee46a0ff318d6fdb01e7
Elena-277 <129676247+Elena-277@users.noreply.github.com>
2023-06-20 14:05:46 -0400

    Added Leave a Message

--- js/index.js
+++ js/index.js
@@ -13,4 +13,37 @@ for (let i = 0; i < skills.length; i++){
     const skill = document.createElement('li');
     skill.innerHTML = `${skills[i]}`;
     skillsList.appendChild(skill);
-}
+};
+
+messageForm = document.querySelector('form[name="leave_message"]');
+messageForm.addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    const name = event.target.usersName.value;
+    const email = event.target.usersEmail.value;
+    const message = event.target.usersMessage.value;
+   
+    console.log(name, email, message);
+
+    const messageSection = document.getElementById("messages");
+    const messageList = messageSection.querySelector("ul");
+    const newMessage = document.createElement("li");
+        
+    newMessage.innerHTML = `<a href = mailto:${email}>${name}</a>
+    <span>${message}</span> `;
+    
+    const removeButton = document.createElement("button");
+    removeButton.innerText = "remove";
+    removeButton.type = "button";
+    removeButton.addEventListener("click", () => {
+            const entry = removeButton.parentNode;
+            entry.remove();
+        });
+
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
+    
+    messageForm.reset();
+});    
+    
